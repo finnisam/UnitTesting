522cda7710d0417260b5a3e11b64dfe63b38e0a7
finnisam <115693748+finnisam@users.noreply.github.com>
2023-04-18 10:46:57 +0530

    Update fraction.js

--- fraction.js
+++ fraction.js
@@ -22,7 +22,10 @@ class fraction
     {
         console.log("%d/%d = %d",this.num/x.num,this.den/x.den,(this.num/x.num)/(this.den/x.den));
     }
-
+    reciprocal()
+    {
+        console.log("%d/%d",this.den,this.num);
+    }
 }
 
 function gcd(a,b)
