4ffe65f6af344541c0fbfc0c84b393983c0e964d
GitKDCollege <114481961+GitKDCollege@users.noreply.github.com>
2023-04-18 09:49:56 +0530

    Update unittesting.js

--- unittesting.js
+++ unittesting.js
@@ -1,1 +1,23 @@
-console.log("SL")
+class fraction
+{
+    constructor(num,den)
+    {
+        this.num = num;
+        this.den = den;
+    }
+
+}
+
+function gcd(a,b)
+{
+    var gcd=1;
+    for(let i=2;(i<=a&&i<=b);i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    return gcd;
+}
+
